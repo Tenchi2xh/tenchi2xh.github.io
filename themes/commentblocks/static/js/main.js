81b9702b4055c730ee6ab7d5aee86aa3a4e9bfea
Hamza Haiken <hamza.haiken@ordina.nl>
2016-02-05 00:23:04 +0100

    fix links styled outside of article, make background lines darker

--- themes/commentblocks/static/js/main.js
+++ themes/commentblocks/static/js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         proximity: 150,
         velocity: [[0.05, 0.1], [0.05, 0.1]],
         dotRadius: [0, 0],
-        lineColor: '#f2f2f2',
+        lineColor: '#f0f0f0',
         lineWidth: 1
     });
     if (navigator.userAgent.indexOf("Firefox") !== -1) {
